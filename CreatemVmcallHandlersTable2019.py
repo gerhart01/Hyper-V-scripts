6fe86d97270a719c7816b4ae39ecdb39a8e8fe48
gerhart01 <25981258+gerhart01@users.noreply.github.com>
2019-09-29 21:49:09 +0300

    Hvix64 handlers functions creator
    IDA PRO script creates hypercalls names in special global table of hvix64.exe

---
+++ CreatemVmcallHandlersTable2019.py
@@ -0,0 +1,308 @@
+__author__ = "Gerhart"
+__license__ = "GPL"
+__version__ = "1.1.0"
+
+#Script parsing and formating structures with hypercall handlers in hvix64.exe 
+#Microsoft doesn't provide symbols for hvix64.exe, therefore i called it VmcallHandlersTable
+#Hypercalls were taken from Hyper-V TLFS, winhvr.sys, winhv.sys, ntoskrnl.exe, securekernel.exe
+#Windows 10 and Windows Server 2019 have different hypercalls. There are not many, but don't forget about it.
+
+import idaapi
+
+TableEntrySize = 0x18 #size of struct for every hypervisor handler
+
+hvcalls_dict = {
+    0x0000: 'HvCallReserved00',
+    0x0001: 'HvSwitchVirtualAddressSpace',
+    0x0002: 'HvFlushVirtualAddressSpace',
+    0x0003: 'HvFlushVirtualAddressList',
+    0x0004: 'HvGetLogicalProcessorRunTime',
+    0x0005: 'HvUpdateHvProcessorFeatures',       #winhvr.sys (09.2019)
+    0x0006: 'HvUnknown01',
+    0x0007: 'HvCondenseMicrocode',               #ntoskrnl.exe, HvDynamicUpdateMicrocode has same microcode
+    0x0008: 'HvNotifyLongSpinWait',
+    0x0009: 'HvParkedVirtualProcessors',
+    0x000a: 'HvDisableVpVtl',                    #winhvr.sys (09.2019)
+	#2016
+    0x000b: 'HvCallSendSyntheticClusterIpi',     #SkpgPatchGuardCallbackRoutine
+    0x000c: 'HvCallModifyVtlProtectionMask',
+    0x000d: 'HvCallEnablePartitionVtl',
+    0x000e: 'HvCallDisablePartitionVtl',
+    0x000f: 'HvCallEnableVpVtl',
+    0x0010: 'HvCallDisableVpVtl',
+    0x0011: 'HvCallVtlCall',
+    0x0012: 'HvCallVtlReturn',
+    0x0013: 'HvCallFlushVirtualAddressSpaceEx',
+    0x0014: 'HvCallFlushVirtualAddressListEx',
+    0x0015: 'HvCallSendSyntheticClusterIpiEx',   #securekernel.exe, SkpgPatchGuardCallbackRoutine
+	#####
+    0x0016: 'HvCallReserved00',
+    0x0017: 'HvLoadHypervisorPatch01',           #securekernel.exe, ShvlLoadHypervisorPatch
+    0x0018: 'HvLoadHypervisorPatch02',           #securekernel.exe, ShvlLoadHypervisorPatch
+    0x0019: 'HvCallReserved00',
+    0x001a: 'HvCallReserved00',
+    0x001b: 'HvUnknown02',
+    0x001c: 'HvReadPerfRegister',                #ntoskrnl.exe, Fast hypercall
+    0x001d: 'HvCallReserved00',
+    0x001e: 'HvCallReserved00',
+    0x001f: 'HvCallReserved00',
+    0x0020: 'HvCallReserved00',
+    0x0021: 'HvCallReserved00',
+    0x0022: 'HvCallReserved00',
+    0x0023: 'HvCallReserved00',
+    0x0024: 'HvCallReserved00',
+    0x0025: 'HvCallReserved00',
+    0x0026: 'HvCallReserved00',
+    0x0027: 'HvCallReserved00',
+    0x0028: 'HvCallReserved00',
+    0x0029: 'HvCallReserved00',
+    0x002a: 'HvCallReserved00',
+    0x002b: 'HvCallReserved00',
+    0x002c: 'HvCallReserved00',
+    0x002d: 'HvCallReserved00',
+    0x002e: 'HvCallReserved00',
+    0x002f: 'HvCallReserved00',
+    0x0030: 'HvCallReserved00',
+    0x0031: 'HvCallReserved00',
+    0x0032: 'HvCallReserved00',
+    0x0033: 'HvCallReserved00',
+    0x0034: 'HvCallReserved00',
+    0x0035: 'HvCallReserved00',
+    0x0036: 'HvCallReserved00',
+    0x0037: 'HvCallReserved00',
+    0x0038: 'HvCallReserved00',
+    0x0039: 'HvCallReserved00',
+    0x003a: 'HvCallReserved00',
+    0x003b: 'HvCallReserved00',
+    0x003c: 'HvCallReserved00',
+    0x003d: 'HvCallReserved00',
+    0x003e: 'HvCallReserved00',
+    0x003f: 'HvCallReserved00',
+    0x0040: 'HvCreatePartition',
+    0x0041: 'HvInitializePartition',
+    0x0042: 'HvFinalizePartition',
+    0x0043: 'HvDeletePartition',
+    0x0044: 'HvGetPartitionProperty',
+    0x0045: 'HvSetPartitionProperty',
+    0x0046: 'HvGetPartitionId',
+    0x0047: 'HvGetNextChildPartition',
+    0x0048: 'HvDepositMemory',
+    0x0049: 'HvWithdrawMemory',
+    0x004A: 'HvGetMemoryBalance',
+    0x004B: 'HvMapGpaPages',
+    0x004C: 'HvUnmapGpaPages',
+    0x004D: 'HvInstallIntercept',
+    0x004E: 'HvCreateVp',
+    0x004F: 'HvDeleteVp',
+    0x0050: 'HvGetVpRegisters',
+    0x0051: 'HvSetVpRegisters',
+    0x0052: 'HvTranslateVirtualAddress',             #used by securekernel.exe
+    0x0053: 'HvReadGpa',
+    0x0054: 'HvWriteGpa',
+    0x0055: 'HvDepricated',                          #depricated
+    0x0056: 'HvClearVirtualInterrupt',
+    0x0057: 'HvCreatePort',
+    0x0058: 'HvDeletePort',
+    0x0059: 'HvConnectPort',
+    0x005A: 'HvGetPortProperty',
+    0x005B: 'HvDisconnectPort',
+    0x005C: 'HvPostMessage',
+    0x005D: 'HvSignalEvent',
+    0x005E: 'HvSavePartitionState',                  #HvCancelSavePartitionState in latest winhvr.sys (09.2019), HvSavePartitionState has same Hypercall ID
+    0x005F: 'HvRestorePartitionState',               #HvCancelRestorePartitionState in latest winhvr.sys (09.2019)
+    0x0060: 'HvInitializeEventLogBufferGroup',
+    0x0061: 'HvFinalizeEventLogBufferGroup',
+    0x0062: 'HvCreateEventLogBuffer',
+    0x0063: 'HvDeleteEventLogBuffer',
+    0x0064: 'HvMapEventLogBuffer',
+    0x0065: 'HvUnmapEventLogBuffer',
+    0x0066: 'HvSetEventLogGroupSources',
+    0x0067: 'HvReleaseEventLogBuffer',
+    0x0068: 'HvFlushEventLogBuffer',
+    0x0069: 'HvPostDebugData',
+    0x006A: 'HvRetrieveDebugData',
+    0x006B: 'HvResetDebugSession',
+    0x006C: 'HvMapStatsPage',
+    0x006D: 'HvUnmapStatsPage',
+    0x006E: 'HvCallMapSparseGpaPages',
+    0x006F: 'HvCallSetSystemProperty',               #HvConfigureProfiler, HvSetHvDebugProperty we can see in winhvr.sys (09.2019)
+    0x0070: 'HvCallSetPortProperty',
+    0x0071: 'HvCallReserved00',
+    0x0072: 'HvCallReserved00',
+    0x0073: 'HvCallReserved00',
+    0x0074: 'HvCallReserved00',
+    0x0075: 'HvCallReserved00',
+    0x0076: 'HvCallAddLogicalProcessor',
+    0x0077: 'HvCallRemoveLogicalProcessor',
+    0x0078: 'HvCallQueryNumaDistance',
+    0x0079: 'HvCallSetLogicalProcessorProperty',
+    0x007A: 'HvCallGetLogicalProcessorProperty',
+    0x007B: 'HvCallGetSystemProperty',               #HvGetSystemInformation in winhvr.sys (09.2019)
+    0x007C: 'HvCallMapDeviceInterrupt',
+    0x007D: 'HvCallUnmapDeviceInterrupt',
+    0x007E: 'HvCallRetargetDeviceInterrupt',         #renamed
+    0x007F: 'HvCallReserved00',                      #made reserved
+    0x0080: 'HvCallMapDevicePages',                  #HvAssertDeviceInterrupt in winhvr.sys (09.2019). HvMapDevicePages is not present.
+    0x0081: 'HvCallUnmapDevicePages',
+    0x0082: 'HvCallAttachDevice',
+    0x0083: 'HvCallDetachDevice',
+    0x0084: 'HvCallEnterSleepState',
+    0x0085: 'HvCallPrepareForSleep',                 #HvNotifyStandbyTransition in winhvr.sys (09.2019) 
+    0x0086: 'HvCallPrepareForHibernate',
+    0x0087: 'HvCallNotifyPartitionEvent',
+    0x0088: 'HvCallGetLogicalProcessorRegisters',
+    0x0089: 'HvCallSetLogicalProcessorRegisters',
+    0x008A: 'HvCallQueryAssociatedLpsforMca',
+    0x008B: 'HvCallNotifyRingEmpty',                 #HvGetNextQueuedPort in winhvr.sys (09.2019)
+    0x008C: 'HvCallInjectSyntheticMachineCheck',
+    0x008d: 'HvCallScrubPartition',
+    0x008e: 'HvCallCollectLivedump',
+    0x008f: 'HvCallDisableHypervisor',
+    0x0090: 'HvCallModifySparseGpaPages',
+    0x0091: 'HvCallRegisterInterceptResult',
+    0x0092: 'HvCallUnregisterInterceptResult',
+	#2016
+    0x0093: 'HvUnknown03',
+    0x0094: 'HvCallAssertVirtualInterrupt',
+    0x0095: 'HvCallCreatePort',
+    0x0096: 'HvCallConnectPort',
+    0x0097: 'HvCallGetSpaPageList',
+    0x0098: 'HvCallReserved00',
+    0x0099: 'HvCallStartVirtualProcessor',
+    0x009A: 'HvCallGetVpIndexFromApicId',
+    0x009B: 'HvUnknown04',
+    0x009C: 'HvUnknown05',
+    0x009D: 'HvUnknown06',
+    0x009E: 'HvUnknown07',
+    0x009F: 'HvUnknown08',
+    0x00A0: 'HvUnknown09',
+    0x00A1: 'HvUnknown10',
+    0x00A2: 'HvUnknown11',
+    0x00A3: 'HvUnknown12',
+    0x00A4: 'HvUnknown13',
+    0x00A5: 'HvUnknown14',
+    0x00A6: 'HvUnknown15',
+    0x00A7: 'HvUnknown16',
+    0x00A8: 'HvUnknown17',
+    0x00A9: 'HvUnknown18',
+    0x00AA: 'HvUnknown19',
+    0x00AB: 'HvUnknown20',
+    0x00AC: 'HvTranslateVirtualAddress01',           #winhvr.sys. Early it has hypercall id 0x52
+    0x00AD: 'HvCheckForIoIntercept',	             #winhvr.sys
+    0x00AE: 'HvSetGpaPageAttributes',                #securekernel.exe
+    0x00AF: 'HvCallFlushGuestPhysicalAddressSpace',
+    0x00B0: 'HvCallFlushGuestPhysicalAddressList',
+        #2019
+    0x00B1: 'HvUnknown21',
+    0x00B2: 'HvAttachDeviceDomain',                  #securekernel.exe
+    0x00B3: 'HvMapDeviceGpaPages',                   #securekernel.exe
+    0x00B4: 'HvUnmapDeviceGpaPages',                 #securekernel.exe
+    0x00B5: 'HvCreateCpuGroup',                      #winhvr.sys
+    0x00B6: 'HvDeleteCpuGroup',                      #winhvr.sys
+    0x00B7: 'HvGetCpuGroupProperty',                 #winhvr.sys
+    0x00B8: 'HvSetCpuGroupProperty',                 #winhvr.sys
+    0x00B9: 'HvGetCpuGroupAffinity',                 #winhvr.sys
+    0x00BA: 'HvHvGetNextCpuGroup',                   #winhvr.sys
+    0x00BB: 'HvGetNextCpuGroupPartition',            #winhvr.sys
+    0x00BC: 'HvUnknown24',
+    0x00BD: 'HvCompleteIntercept',                   #winhvr.sys
+    0x00BE: 'HvPrecommitGpaPages',                   #winhvr.sys
+    0x00BF: 'HvUncommitGpaPages',                    #winhvr.sys
+    0x00C0: 'HvCallReserved00',
+    0x00C1: 'HvCallReserved00', 
+    0x00C2: 'HvDispatchVp',                          #winhvr.sys, fast hypercall
+    0x00C3: 'HvUnknown25', 
+    0x00C4: 'HvHandleDmaTargetExposeToVtl0',         #securekernel.exe SkpnppHandleDmaTargetExposeToVtl0
+    0x00C5: 'HvUnknown26',
+    0x00C6: 'HvCallReserved00',
+    0x00C7: 'HvUnknown27',
+    0x00C8: 'HvUnknown28', 
+    0x00C9: 'HvGetGpaPagesAccessState',              #winhvr.sys
+    0x00CA: 'HvUnknown29',
+    0x00CB: 'HvCallReserved00', 
+    0x00CC: 'HvQueryVtlProtectionMaskRange',         #winhvr.sys
+    0x00CD: 'HvModifyVtlProtectionMaskRange',        #winhvr.sys
+    0x00CE: 'HvCallReserved00',
+    0x00CF: 'HvCallReserved00',
+    0x00D1: 'HvCallReserved00', 
+    0x00D2: 'HvAcquireSparseGpaPageHostAccess',      #winhvr.sys
+    0x00D3: 'HvReleaseSparseGpaPageHostAccess',      #winhvr.sys
+    0x00D4: 'HvCheckSparseGpaPageVtlAccess',         #winhvr.sys
+    0x00D5: 'HvCallReserved00',
+    0x00D6: 'HvCallReserved00',
+    0x00D7: 'HvAcquireSparseSpaPageHostAccess',      #winhvr.sys
+    0x00D8: 'HvReleaseSparseSpaPageHostAccess',      #winhvr.sys
+    0x00D9: 'HvModifySparseGpaPageHostVisibility',   #winhv.sys
+    0x00DA: 'HvCallReserved00', 
+    0x00DB: 'HvAcceptGpaPages',                      #winhvr.sys
+    0x00DC: 'HvUnknown30',
+    0x00DD: 'HvUnknown31',
+    0x00DE: 'HvUnknown32',
+    0x00DF: 'HvUnknown33', 
+    0x00E0: 'HvQueryDeviceInterruptTarget',          #winhvr.sys
+    0x00E1: 'HvMapVpRegisterPage',                   #winhvr.sys (HvMapVpStatePage in Windows 10)
+    0x00E2: 'HvUnknown34', 
+    0x00E3: 'HvGetXsaveData',                        #winhvr.sys
+    0x00E4: 'HvSetXsaveData',                        #winhvr.sys
+    0x00E5: 'HvGetLocalInterruptControllerState',    #winhvr.sys
+    0x00E6: 'HvSetLocalInterruptControllerState',    #winhvr.sys
+    0x00E7: 'HvCreateIptBuffers',                    #winhvr.sys (Windows 10)
+    0x00E8: 'HvDeleteIptBuffers',                    #winhvr.sys (Windows 10)
+}
+
+hvStats = GetEnum("HV_HYPERCALL_STATS_GROUP") #hypercall's category using for statistic purpose
+if hvStats != 0xffffffffffffffffL:
+    print "enum HV_HYPERCALL_STATS_GROUP already exists"
+if hvStats == 0xffffffffffffffffL:
+    hvStats = AddEnum(-1, "HV_HYPERCALL_STATS_GROUP", 0)
+    BeginTypeUpdating(UTP_ENUM)
+    AddConst(hvStats,"GPA_SPACE_HYPERCALL",0x3D)
+    AddConst(hvStats,"LOGICAL_PROCESSOR_HYPERCALL",0x3E)
+    AddConst(hvStats,"LONG_SPIN_WAIT_HYPERCALL",0x3F)
+    AddConst(hvStats,"OTHER_HYPERCALL",0x40)
+    AddConst(hvStats,"INTER_PARTITION_COMMUNICATION_HYPERCALL",0x41)# i don't see same counter in PerformanceMonitor, but there is category in Hyper-v TLFS.
+    AddConst(hvStats,"VIRTUAL_INTERRUPT_HYPERCALL",0x42)
+    AddConst(hvStats,"VIRTUAL_MMU_HYPERCALL",0x43)
+    AddConst(hvStats,"VIRTUAL_PROCESSOR_HYPERCALL",0x44)
+    AddConst(hvStats,"VIRTUAL_PROCESSOR_HYPERCALL02",0x45)
+    AddConst(hvStats,"FLUSH_PHYSICAL_ADDRESS_SPACE",0x8F)
+    AddConst(hvStats,"FLUSH_PHYSICAL_ADDRESS_LIST",0x90)
+    EndTypeUpdating(UTP_ENUM)
+
+StartAddress = idaapi.get_segm_by_name("CONST").startEA #usually start of CONST segment, but it will can change in future
+print "Address of HypercallHandlerTable is ",hex(StartAddress)
+#hvCallCount = 0x92 #count of hypercalls in Windows Server 2012 R2
+#hvCallCount = 0xAD count of hypercalls in Windows Server 2016 TP4
+#hvCallCount = 0xBC #count of hypercalls in Windows Server 2016
+#hvCallCount = 0xE6 #count of hypercalls in Windows Server 2019
+#hvCallCount = 0xE8 #count of hypercalls in Windows 10 19H1
+hvCallCount = len(hvcalls_dict)
+
+print "hvcalls_dict size is",hex(hvCallCount)
+
+#Working with first element manually
+
+HvCallReserved00 = idc.Qword(StartAddress)
+del_items(HvCallReserved00, flags=0, size=1)
+idc.MakeCode(HvCallReserved00)
+idc.MakeFunction(HvCallReserved00)
+MakeNameEx(HvCallReserved00,str('HvCallReserved00'), SN_NOWARN)
+for j in range(0,6):
+    idc.MakeWord(StartAddress+8+j*2)
+j=j+1
+idc.MakeDword(StartAddress+8+j*2)
+OpEnumEx(StartAddress+8+j*2, 0, hvStats, 0)
+
+#Next elements
+for i in range(1,hvCallCount+1):
+    hvCallAddress = idc.Qword(StartAddress+i*TableEntrySize)
+    if (hvCallAddress !=HvCallReserved00):
+        idc.MakeCode(hvCallAddress)
+        idc.MakeFunction(hvCallAddress)
+        MakeNameEx(hvCallAddress,hvcalls_dict[i], SN_NOWARN)
+    for j in range(0,6):
+        dw_addr = idc.MakeWord(StartAddress+i*TableEntrySize+8+j*2)
+    j=j+1
+    idc.MakeDword(StartAddress+i*TableEntrySize+8+j*2)
+    OpEnumEx(StartAddress+i*TableEntrySize+8+j*2, 0, hvStats, 0)
+
